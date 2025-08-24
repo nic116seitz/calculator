7e222ecc2dd428e2c0e1bb0fe3edad3f59e13412
nic116seitz <25190512+nic116seitz@users.noreply.github.com>
2025-08-24 09:26:19 -0600

    Sync This commit is to sync repo for work on a different device

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let numA = '';
 let numB = '';
 
-const input = document.querySelectorAll('.num');
+const num = document.querySelectorAll('.num');
 const operator = document.querySelector('.oper');
 const calc = document.querySelector('#calViewPort');
 const para = document.createElement('p');
 
-// num.forEach(e => {
-//   e.addEventListener('click', (target) => {
-//     para.textContent += target.textContent
-//     console.log(target.textContent)
-//   })
-//});
+num.forEach(e => {
+   e.addEventListener('click', (target) => {
+     para.textContent += target.textContent
+     console.log(target.textContent)
+   })
+});
+
 // This is a possible way to create an operate function
 // function operate(numA, numB, operation){
 //   if (operation == "add" || equals == true) {
