ceaacd4993e01af6282b79cf966aa9bbb9bb6982
nic116seitz <25190512+nic116seitz@users.noreply.github.com>
2025-08-24 09:14:20 -0600

    Current Version This commit is so that the repo is in its current form

--- script.js
+++ script.js
@@ -1,47 +1,33 @@
-const calNums = document.querySelector("calNums");
-let numNum = 1;
-const num = document.createElement("div");
+let numA = '';
+let numB = '';
 
+const input = document.querySelectorAll('.num');
+const operator = document.querySelector('.oper');
+const calc = document.querySelector('#calViewPort');
+const para = document.createElement('p');
 
-
-function numListener(){
-  const numSelect = document.querySelectorAll(".num");
-  numSelect.forEach((numb) => {
-    sqr.addEventListener("click", (event) => {
-
-    })
-  })
-}
-
-let calculator = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
-  zero: 0,
-  operation: [ "add", "subtract", "multiply", "divide", "equals", "removLast", "clear" ]
-}
-
-function operate(numA, numB, operation){
-  if (operation == "add" || equals == true) {
-    const sum = numA + numB;
-    return sum;
-  }
-  else if (operation == "subtract") {
-    const diff = numA - numB;
-    return diff;
-  }
-  else if (operation == "multiply") {
-    const prod = numA * numB;
-    return prod;
-  }
-  else if (operation == "divide") {
-    const quot = numA / numB;
-    return quot;
-  }
-}
+// num.forEach(e => {
+//   e.addEventListener('click', (target) => {
+//     para.textContent += target.textContent
+//     console.log(target.textContent)
+//   })
+//});
+// This is a possible way to create an operate function
+// function operate(numA, numB, operation){
+//   if (operation == "add" || equals == true) {
+//     const sum = numA + numB;
+//     return sum;
+//   }
+//   else if (operation == "subtract") {
+//     const diff = numA - numB;
+//     return diff;
+//   }
+//   else if (operation == "multiply") {
+//     const prod = numA * numB;
+//     return prod;
+//   }
+//   else if (operation == "divide") {
+//     const quot = numA / numB;
+//     return quot;
+//   }
+// }
