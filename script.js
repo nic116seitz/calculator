63054ad1a6babf8543d5b31455e79e5fb0a4b46e
nic116seitz <25190512+nic116seitz@users.noreply.github.com>
2025-09-13 08:36:50 -0600

    Sync This is a sync commit This is to sync the repo so that it may be  edited from my laptop

--- script.js
+++ script.js
@@ -8,24 +8,32 @@ const para = document.createElement('p');
 const operAll = document.querySelectorAll('.oper');
 const equals = document.querySelector('#equals');
 const clear = document.querySelector('#clear');
+let currentOper = '';
 
 num.forEach(e => {
    e.addEventListener('click', event => {
      para.textContent += event.target.textContent
      console.log(event.target.textContent);
      calc.appendChild(para)
+     if (para.textContent != '') {
+      clear.textContent = 'C'
+    }
    })
 });
 
 function clearView(){
+ if (clear.textContent = 'C') {
   calc.innerHTML = '';
   para.textContent = '';
+ }
+
+ else if (clear.textContent = 'AC') {
+    numA = '';
+    console.log(numA);
+ }
 }
 
-
 clear.addEventListener('click', clearView);
-
-let currentOper = '';
 
 operAll.forEach(e => {
   e.addEventListener('click', event => {
@@ -34,9 +42,9 @@ operAll.forEach(e => {
     console.log(`this is currentOper ${currentOper}`);
     console.log(`this is numA ${numA}`);
     clearView();
+    clear.textContent = 'AC'
   });
 })
-
 
 // This is a possible way to create an operate function
  function operate(num1, num2, operation){
